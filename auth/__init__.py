a3faa7f6032a8f5bd7cc703c052dccba646c68e4
Victorvs1967 <victorsmirnov@me.com>
2023-06-16 15:37:05 +0300

    fix token expierense

--- auth/__init__.py
+++ auth/__init__.py
@@ -9,6 +9,7 @@ from services import create_user, authenticate, is_exist
 
 # secret string generated with code: node -e "console.log(require('crypto').randomBytes(256).toString('base64'));"
 app.config['JWT_SECRET_KEY'] = 'FxONLsEWZwMWVaSE5fngTdlvlRIHseTNSQz4xD1wR9gPn0cq9LBIm8os3NI7wlhKLcNjm9OwGYRRehXDMLqIqVBXZ1u+IbVaBh545dV8a/+4ZtifVMhicuexAJOJ4ST1GrfTCMnTlwNPcIDsTqCblryeyLSrpUyi3EZnwxEIjC87AIdUoRY+XAfOLABWjP+/uEQ8EcfzcRti3wHkusaa5yiYkXvvrPRsdcrzAFxRm3bHfutVbRDtvBHoKYTGHWw6VKmHitMPB+S6wz1LTa2GPDq/xObP6RH9nvoUEcY9oNa/TWrzo8gU9wQs0zOwNgGcfr6nsgWXfRtqVkFD6KnRjg=='
+app.config['JWT_ACCESS_TOKEN_EPIRES'] = 86400
 jwt = JWTManager(app)
 CORS(app)
 
@@ -20,8 +21,8 @@ class Login(Resource):
     if user:
       token = create_access_token(identity={
         'username': username,
-        'role': 'admin',
-      }, expires_delta=False)
+        'role': 'ADMIN',
+      })
       return { 'token': token }
     return { 'error': 'Invalid username or password' }
 
