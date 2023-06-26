02c871fe86602870b7ef5c3992df84e606065b87
Victorvs1967 <victorsmirnov@me.com>
2023-06-26 09:48:35 +0300

    fix models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,22 +3,14 @@ from werkzeug.security import generate_password_hash
 
 class User:
   def __init__(self, username: str, password: str, email: str, first_name: str, last_name: str):
-    # self._id = str(uuid1().hex)
     self.username = username
     self.password = generate_password_hash(password)
     self.email = email
     self.first_name = first_name
     self.last_name = last_name
 
-  def set_id(self, id):
-    self._id = id
-
 class Book:
   def __init__(self, title: str, author: str, year: str):
-    # self._id = str(uuid1().hex)
     self.title = title
     self.author = author
     self.year = year
-
-  def set_id(self, id):
-    self._id = id
